b26b134fd5f8d9c6d2fe2b89f9c0651bc9496bd5
Cat Gomez <catherinedgomez@gmail.com>
2020-03-16 04:49:09 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,8 @@ def initialize(emails)
 end
 
 def parse(emails)
-
+  emails
+  return emails
 #   email_array = @email.split(/[, ]/).uniq
 # email_array.reject! {|element| element.empty?}
 # email_array
